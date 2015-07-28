6693219aa3a471da8548ed4462b9018209799267
Razvan Chitu <razvan.chitu@eaudeweb.ro>
2015-07-28 11:00:47 +0300

    Refs #27397 - Updated starting script to support signal handling

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+_terminate() {
+	$ZEO_HOME/bin/zeoserver stop
+	kill -TERM $child 2>/dev/null
+}
+
+trap _terminate SIGTERM SIGINT
+
 if ! test -e $ZEO_HOME/buildout.cfg; then
     python /configure.py
 fi
@@ -8,4 +15,8 @@ if test -e $ZEO_HOME/buildout.cfg; then
     $ZEO_HOME/bin/buildout -c $ZEO_HOME/buildout.cfg
 fi
 
-$ZEO_HOME/bin/zeoserver start && exec $ZEO_HOME/bin/zeoserver logtail
+$ZEO_HOME/bin/zeoserver start
+$ZEO_HOME/bin/zeoserver logtail &
+
+child=$!
+wait "$child"
